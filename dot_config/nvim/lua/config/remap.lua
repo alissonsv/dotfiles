9277e374ccde79e94aec76d5631412a28203cac8
Alisson Vargas <contato@alissonsv.com>
2024-01-06 14:49:06 -0300

    Initial Commit

---
+++ dot_config/nvim/lua/config/remap.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = " "
+
+-- move text using ALT
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv-gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv-gv")
+vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==")
+vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==")
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+
+vim.keymap.set('n', '<C-Up>', ':resize -2<cr>')
+vim.keymap.set('n', '<C-Down>', ':resize +2<cr>')
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<cr>')
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<cr>')
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('', '<C-h>', '<C-w>h')
+vim.keymap.set('', '<C-l>', '<C-w>l')
+
+vim.keymap.set('', '<leader>n', ':bnext<cr>')
+vim.keymap.set('', '<leader>p', ':bprevious<cr>')
+vim.keymap.set('', '<leader>d', ':bdelete<cr>')
+
+vim.keymap.set('n', '<leader>t', ':ToggleTerm<cr>')
---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,120 @@
+return {
+	{ 
+		'projekt0n/github-nvim-theme',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd('colorscheme github_dark_dimmed')
+		end
+	},
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		init = function()
+			require 'config.telescope'
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		init = function()
+			require 'config.treesitter'
+		end
+	},
+	{
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require 'config.gitsigns'
+		end
+	},
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		priority = 1000,
+		branch = 'v3.x',
+	},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-path'},
+	{'hrsh7th/cmp-nvim-lua'},
+	{'hrsh7th/cmp-buffer'},
+	{'L3MON4D3/LuaSnip'},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require('config.nvim_tree')
+		end,
+	},
+	{ 'wakatime/vim-wakatime' },
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup {
+				options = { theme = 'ayu_mirage' }
+			}
+		end
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 1000
+		end,
+		opts = {
+			mode = "n",
+			prefix = "<leader>",
+			buffer = nil,
+			silent = true,
+			noremap = true,
+			nowait = true
+		},
+		vopts = {
+			mode = "v",
+			prefix = "<leader>",
+			buffer = nil,
+			silent = true,
+			noremap = true,
+			nowait = true
+		}
+	},
+	{
+		'ahmedkhalf/project.nvim',
+		opts = {
+			manual_mode = false
+		},
+		event = 'VeryLazy',
+		config = function (_, opts)
+			opts.detection_methods = { "lsp", "pattern" }
+			opts.patterns = {
+				".git",
+				".hg",
+				".svn",
+			}
+			require("project_nvim").setup(opts)
+			require("telescope").load_extension("projects")
+		end,
+		keys = {
+			{ "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+		},
+	},
+	{'akinsho/toggleterm.nvim', version = "*", config = true},
+	{
+		'akinsho/bufferline.nvim',
+		version = '*',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons'
+		},
+		config = function()
+			require("config.bufferline")
+		end
+	}
+}
