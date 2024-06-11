219135dd3424c0784e4c5adb4fd848a32be0a12b
Alisson Vargas <contato@alissonsv.com>
2024-06-11 20:23:15 -0300

    update plugins and remove hardtime

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -206,18 +206,6 @@ require('lazy').setup({
     'rcarriga/nvim-notify'
   },
 
-  -- workflow practices
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'nvim-lua/plenary.nvim'
-    },
-    opts = {
-      max_count = 5
-    }
-  },
-
   -- tabs
   {
     'akinsho/bufferline.nvim',
